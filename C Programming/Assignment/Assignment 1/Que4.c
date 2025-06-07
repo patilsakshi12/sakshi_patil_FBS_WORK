4b08ae96e45f685dc3a6286b98fdaa0728994d6d
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-07 10:41:27 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 1/Que4.c
@@ -0,0 +1,11 @@
+void main(){
+	char ch='e';
+	if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+		printf("Vowel\n");
+	}
+	else{
+		printf("consont\n");
+	}
+	printf("%d",ch);
+	
+}
