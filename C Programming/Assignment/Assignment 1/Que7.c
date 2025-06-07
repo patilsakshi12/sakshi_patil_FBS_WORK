e84ec1abda747ce2754e02f091f2b6ecd2c3251f
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-07 10:44:49 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 1/Que7.c
@@ -0,0 +1,18 @@
+void main(){
+	double basic=1500, da,ta,hra,total;
+	if(basic<=5000){
+		da=basic*0.10;
+		ta=basic*0.20;
+		hra=basic*0.25;
+	}
+	else{
+		da=basic*0.15;
+		ta=basic*0.25;
+		hra= basic*0.30;
+	}
+	 total=basic+da+ta+hra;
+	printf("total salary is %.2lf",total);
+}
+
+
+
