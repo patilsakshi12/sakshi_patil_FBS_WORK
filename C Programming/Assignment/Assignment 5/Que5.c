0924725e9abac7de255c6793d12d01832310f416
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-16 18:37:37 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 5/Que5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int i,j,n;
+	printf("Enter a number");
+	scanf("%d",&n);
+	for(i=n;i>=1;i--)
+	{
+		for(j=n;j>=i;j--){
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+}
