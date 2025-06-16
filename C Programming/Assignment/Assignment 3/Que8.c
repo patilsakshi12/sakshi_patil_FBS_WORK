1bac4fdc99163bea43e2facba26030d235048dcf
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-16 09:24:48 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 3/Que8.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int n, rem,sum=0;
+	printf("Enter a number");
+	scanf("%d",&n);
+	int temp=n;
+	while(n>0){
+		rem=n%10;
+		int fact=1;
+		while(rem>=1){
+			fact=fact*rem;
+			rem--;
+		}
+		sum=sum+fact;
+		n=n/10;
+	}
+	if(sum==temp)
+	printf("Strong no");
+	else
+	printf("Not strong no");
+	
+	
+}
