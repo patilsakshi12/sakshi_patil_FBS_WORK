c6f61ca0e0ac2dab99c49d7463d085ed6f44d188
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-18 18:46:31 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 4/Prime.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+	int start,end;
+	printf("Enter a starting value");
+	scanf("%d",&start);
+	printf("Enter a ending value");
+	scanf("%d",&end);
+	int j;
+	for(j=start;j<=end;j++)
+	{
+	
+	int n=j,flag=0;
+//	printf("Enter a number");
+//	scanf("%d",&n);
+	int i;
+	for(i=2;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			flag =1;
+			break;
+		}
+	}
+	if(flag==0)
+	printf("%d ",j);
+}
+}
