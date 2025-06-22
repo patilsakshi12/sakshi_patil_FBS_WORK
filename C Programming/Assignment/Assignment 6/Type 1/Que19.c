5f5200b6d62968cbda8ce9dca2c99f31a8d5d858
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-22 18:59:25 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type 1/Que19.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void checkArmstrong(); //declare
+void main()
+{
+      checkArmstrong();	
+}
+void checkArmstrong(){
+
+	int num=153, rem, sum=0;
+	int temp=num;
+	while(num>0){
+	   rem=num%10;
+	   sum=sum+(rem*rem*rem);
+	   num=num/10;
+	}
+	
+	if(sum==temp)
+	printf("Armstrong no");
+	else
+	printf("Not Armstrong");
+}
