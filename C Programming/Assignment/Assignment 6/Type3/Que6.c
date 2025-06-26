08d2f2eb3f9170ef1cd2914daab4ef8a25a83c2c
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:48:25 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/Que6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void totalSalary(double); // declare
+void main(){
+	double basic;
+	printf("Enter a basic salary");
+	scanf("%lf",&basic);
+	totalSalary(basic); // call
+}
+void totalSalary(double basic) // defination
+{
+	double da,ta,hra,total;
+	if(basic<=5000){
+		da=basic*0.10;
+		ta=basic*0.20;
+		hra=basic*0.25;
+	}
+	else{
+		da=basic*0.15;
+		ta=basic*0.25;
+		hra= basic*0.30;
+	}
+	 total=basic+da+ta+hra;
+	printf("total salary is %.2lf",total);
+}
