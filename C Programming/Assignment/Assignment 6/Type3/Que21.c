2b3081278ab8711d3d241fb79de639b42b9b75bd
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:58:54 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/Que21.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void checkStrong(int);
+void main()
+{
+	int num;
+	printf("Enter a number");
+	scanf("%d",&num);
+	checkStrong(num); // call
+}
+void checkStrong(int n)
+{
+
+	int rem,sum=0;
+	int temp=n;
+	while(n>0){
+		rem=n%10;
+		int fact=1;
+		while(rem>=1){
+			fact=fact*rem;
+			rem--;
+		}
+		sum=sum+fact;
+		n=n/10;
+	}
+	if(sum==temp)
+	printf("Strong no");
+	else
+	printf("Not strong no");
+	
+	
+}
