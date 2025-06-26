212956a75ef8ec0ec68c19716eaa3113dd2c8bdb
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:58:21 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/Que20.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void isFact(int);
+void main()
+{
+	int n;
+	printf("Enter a number");
+	scanf("%d",&n);
+	isFact(n);
+}
+void isFact(int num)
+{
+	int i,fact=1;
+	for(i=1;i<=num;i++)
+	{
+		fact=fact*i;
+	}
+	printf("Factorial %d",fact);
+	
+}
+
