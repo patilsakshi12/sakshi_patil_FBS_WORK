8a4c4503389f3b273eac55e61003022b2ce3ff6d
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:47:51 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/Que4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void EligibleVote(int); //declartion
+void main()
+{  int age;
+printf("Enter a age");
+scanf("%d",&age);
+   EligibleVote(age); //call
+}
+void EligibleVote(int age) //defination
+{
+	
+	if(age>=18){
+		printf("Eligibal for vote");
+	}
+	else{
+		printf("Not Eligibal for vote");
+	}
+}
