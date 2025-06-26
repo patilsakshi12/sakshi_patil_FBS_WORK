1f8804c45d9ea09dc12e78902fa7995890f9b8d1
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:59:38 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/Que22.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void palindrome(int );
+void main()
+{
+	int n;
+	printf("Enter a number");
+	scanf("%d",&n);
+	palindrome(n);
+}
+void palindrome(int num)
+{
+
+	int rem, rev=0;
+
+	int temp=num;
+	while(num>0){
+		rem=num%10;
+		rev=rev*10+rem;
+		num=num/10;
+	}
+	if(rev==temp)
+	printf("number is Palindrome");
+	else
+	printf("number is not palindrome");
+	
+	
+}
