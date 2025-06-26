94ab68558650db56de81b0116e8eb7aab42bee47
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:43:37 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/Add.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void sum(int,int); //function declaration
+void main()
+{
+	int n1,n2;
+	printf("Enter a number");
+	scanf("%d%d",&n1,&n2);
+	sum(n1,n2);// function call and actual parameter
+	
+	
+}
+ void sum(int x,int y) //formal parameter
+{
+      // function defination
+	int res=x+y;
+	printf("Sum of two number %d",res);
+	
+	
+}
