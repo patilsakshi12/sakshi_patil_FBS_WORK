ff45d7c1be7a125fd8426fffa51218bd21bba847
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:53:37 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/Que13.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void Checkage(int);
+void main(){
+	int age;
+	printf("Enter your age");
+	scanf("%d",&age);
+	Checkage(age); // call
+}
+void Checkage(int age){
+
+	
+	if(age<12)
+	printf("You are a Child");
+	else if(age>=12 && age<=19)
+	printf("You are a Teenager");
+	else if(age>=20 && age<=59)
+	printf("You are a Adult");
+	else if(age>=60)
+	printf("You are senior");
+	else
+	printf("Invalid age");
+	
+}
