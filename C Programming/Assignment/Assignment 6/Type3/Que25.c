80996b16c176a3fc12d41bc9479535a4754232b8
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 18:02:29 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/Que25.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void perfectNum(int,int);
+void main()
+{
+	int start,end;
+	printf("Enter a Starting value");
+	scanf("%d",&start);
+	printf("Enter a ending value");
+	scanf("%d",&end);
+	perfectNum(start,end);
+}
+void perfectNum(int start,int end){
+
+	
+	int j;
+	for(j=start;j<=end;j++)
+	{
+	
+	int n=j,sum=0;
+	//printf("Enter a number");
+//	scanf("%d",&n);
+	int i;
+	for(i=1;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			sum=sum+i;
+		}
+	}
+	if(sum==n)
+	printf("%d ",sum);
+}
+
+}
