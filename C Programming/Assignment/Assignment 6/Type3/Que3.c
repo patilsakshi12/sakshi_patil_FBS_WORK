93010eea9aac018e02577756694523cf4f719ddc
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:46:37 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/Que3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void Vowel(char); // declartion
+void main()
+{ char ch;
+	Vowel('X'); //call
+}
+void Vowel(char ch)     // defination
+{
+	
+	if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+		printf("Vowel");
+	}
+	else{
+		printf("consont");
+	}
+
+	
+}
