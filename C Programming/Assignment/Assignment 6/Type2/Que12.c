2fc75a7d361dd813b6b4139db3f47c810d8ac482
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:37:03 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type2/Que12.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int checkdivisible();
+void main()
+{
+int res=checkdivisible();
+if(res==1)
+printf("Divisible by both\n");
+else if(res==2)
+printf("Divisible by 3 but not by 5\n");
+else if(res==3)
+printf("Divisible by 5 but not by 3\n");
+else
+printf("Divisible by none");
+}
+
+
+
+int checkdivisible()
+{
+	int num;
+	printf("Enter the number");
+	scanf("%d",&num);
+	if(num%3==0 && num%5==0)
+	return 1;
+	else if(num%3==0 && num%5 !=0)
+	return 2;
+	else if(num%5==0 && num%3 !=0)
+	return 3;
+	else
+	return 0;
+}
