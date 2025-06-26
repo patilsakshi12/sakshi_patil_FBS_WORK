0ae2c14e40cb2a44468739b99f31aac310e843c3
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:44:58 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/evenodd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void evenodd(int); //declartion
+void main(){
+	int num;
+	printf("Enter a number"); // definition
+	scanf("%d",&num);
+	evenodd(num); // function call
+	
+}
+void evenodd(int num){ //defination
+	
+	if(num%2==0)
+	{
+		printf("Even");
+	}
+	else{
+		printf("odd");
+	}
+}
+
