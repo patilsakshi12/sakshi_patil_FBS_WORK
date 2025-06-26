3de4169a968b9b47b9fb29fa5b0f0e1b67e11854
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:36:20 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type2/Que10.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int checkMarks();
+
+void main()
+{
+	int res=checkMarks(); // call
+	if(res==1)
+	printf("Distinction");
+	else if(res==2)
+	printf("First class");
+	else if(res==3)
+	printf("Second class");
+	else if(res==4)
+	printf("pass");
+	else
+	printf("Fail");
+}
+int checkMarks()
+{
+	int marks;
+	printf("Enter the marks");
+	scanf("%d", &marks);
+	
+	if(marks>75)
+	 return 1;
+	else if(marks>65)
+     return 2;
+	else if(marks>55)
+	return 3;
+	else if(marks>=40)
+	return 4;
+	else
+	return 0;
+}
+
