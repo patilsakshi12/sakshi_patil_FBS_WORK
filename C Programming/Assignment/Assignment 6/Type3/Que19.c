923d94599df3d022acaf0c03f1f6dc69384bf15f
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 17:57:47 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/Que19.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void isPerfect(int);
+void main()
+{
+	int n;
+	printf("Enter a number");
+	scanf("%d",&n);
+	isPerfect(n);
+}
+void isPerfect(int num)
+{
+	int i=1,sum=0;
+	while(i<num)
+	{
+		if(num%i==0)
+		{
+			sum=sum+i;
+		}
+		i++;
+	}
+	if(sum==num)
+	printf("Number is perfect");
+	else
+	printf("Numbet is not perfect");
+}
