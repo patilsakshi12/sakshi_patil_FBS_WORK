ef5840f7a07889cb39c555c5e7cd0b84a311db99
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-26 18:03:00 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type3/Que26.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void strong(int,int);
+void main()
+{
+	int start,end;
+	printf("Enter a starting value");
+	scanf("%d",&start);
+	printf("Enter a ending value");
+	scanf("%d",&end);
+	strong(start,end);
+}
+void strong(int start,int end){
+
+	
+	int j;
+	for(j=start;j<=end;j++){
+	
+	int num=j, rem, sum=0;
+	//printf("Enter a NUmber");
+	//scanf("%d",&num);
+	int temp;
+	for(temp=num;num>0;num=num/10)
+	{
+		rem=num%10;
+	    int fact=1,i;
+		for(i=1;i<=rem;i++){
+			fact=fact*i;
+		}
+		sum=sum+fact;
+	}
+	if(sum==temp)
+	printf("%d ",sum);
+}
+}
