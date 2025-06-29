3236a3d93baaca21c4e0726c74a20233ca0d8de9
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2025-06-29 21:27:22 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 6/Type4/Que11.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+double discount(double);
+void main()
+{
+	double price;
+	printf("Enter the price");
+	scanf("%lf",&price);
+double res=discount(price); //call
+printf("Discount = %.2lf\n",res);
+}
+double discount(double price)
+{
+		double discount;
+	char isstudent;
+	printf("Are you student (Y/N)");
+	scanf(" %c",&isstudent);
+	
+	if(isstudent== 'y' || isstudent== 'Y'){
+		if(price> 500)
+		{
+			discount= price*0.20;
+		}
+		else
+		{
+			discount= price*0.10;
+		}
+	}
+	else
+	{
+		if(price>600)
+		{
+			discount = price*0.15;
+		}
+		else
+		{
+			discount = 0;
+		}
+	}
+	printf("price=%2.lf\n",price);
+	printf("Final amount to pay: %.2lf\n",price-discount);
+    return discount;
+}
+
