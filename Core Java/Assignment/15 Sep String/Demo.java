dcb14e3f240e33adcd413c8c731270c03abb3d5f
patilsakshi12 <130861757+patilsakshi12@users.noreply.github.com>
2026-01-18 22:00:11 +0530

    Add files via upload

---
+++ Core Java/Assignment/15 Sep String/Demo.java
@@ -0,0 +1,14 @@
+class StringDemo
+{
+   public static void main(String args[])
+  {
+
+    String str1="Hello";
+    String str2=new String("Hello");
+    String str3="Hello";
+   System.out.println(str1==str3);
+   System.out.println(str1==str2);
+    System.out.println(str1.equals(str2));
+  }
+
+}
